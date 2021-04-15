4012018e8c8b4ad8fe8df0ee41df9f337a064acd
Justice Dunn <justicedunn@me.com>
2021-04-14 20:59:52 -0600

    Finish skeleton of clox

---
+++ src/clox/chunk.h
@@ -0,0 +1,27 @@
+// "chunk" is how we refer to a sequence of bytecode
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+    OP_CONSTANT,
+    OP_RETURN,
+} OpCode;
+
+// Dynamic array to hold bytecode instructions
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t* code;
+    int* lines;
+    ValueArray constants;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
+
+#endif
